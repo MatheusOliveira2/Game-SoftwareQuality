840d6b8242daba9630e6c8a0ef45f4abbb4d11d4
Matheus Oliveira <matheusalmeida.soliveira@gmail.com>
2021-05-30 12:17:14 -0300

    Dialog boxes

---
+++ Levels/Panel2.gd
@@ -0,0 +1,46 @@
+extends Panel
+
+var pag = 1
+
+var perc = null
+
+var mensagem
+
+func _ready():
+	pass
+
+func _process(delta):
+	
+	if pag == 1:
+		$previous.hide();
+	else:
+		$previous.show();
+	
+	
+	if pag == 1:
+		mensagem = "dois"
+	if pag == 2:
+		mensagem = "dois page dois"
+	$RichTextLabel.set_text(str(mensagem))
+	
+
+
+func _on_next_pressed():
+	pag += 1
+	print(pag)
+	pass # Replace with function body.
+
+
+func _on_previous_pressed():
+	pag -= 1
+	pass # Replace with function body.
+
+
+func _on_Area2D_body_entered(body):
+	show()
+	pass # Replace with function body.
+
+
+func _on_Area2D_body_exited(body):
+	hide()
+	pass # Replace with function body.
